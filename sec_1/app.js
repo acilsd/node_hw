9ffdf579bcf48bf15f745b2e4a644ad0080124c1
acilsd <acilsd@derpy.ru>
2019-01-03 00:43:15 +0300

    Ehhh routing requests?

--- sec_1/app.js
+++ sec_1/app.js
@@ -1,11 +1,19 @@
 const http = require('http');
 const PORT = '8087';
 
-const rqListener = (req, res) => {
-  console.log(req, res); 
-};
+// const rqListener = (req, res) => {
+//   console.log(req, res); 
+// };
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>index</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></input></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
   // console.log(req, res);
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
