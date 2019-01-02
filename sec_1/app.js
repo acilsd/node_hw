5694ada27ac48b363559bbda0b82440c5e107d8f
acilsd <acilsd@derpy.ru>
2019-01-03 00:37:48 +0300

    Res write.

--- sec_1/app.js
+++ sec_1/app.js
@@ -8,6 +8,11 @@ const rqListener = (req, res) => {
 const server = http.createServer((req, res) => {
   // console.log(req, res);
   res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>this looks really ugly</title></head>');
+  res.write('<body><h1>hello world</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(PORT);
