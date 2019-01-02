f126009841a7b408142f97643d16cc7ed6b97b29
acilsd <acilsd@derpy.ru>
2019-01-03 00:34:39 +0300

    Setup linter.

--- sec_1/app.js
+++ sec_1/app.js
@@ -2,11 +2,12 @@ const http = require('http');
 const PORT = '8087';
 
 const rqListener = (req, res) => {
-    console.log(req, res); 
+  console.log(req, res); 
 };
 
 const server = http.createServer((req, res) => {
-    console.log(req, res);
+  // console.log(req, res);
+  res.setHeader('Content-Type', 'text/html');
 });
 
 server.listen(PORT);
