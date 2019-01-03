e9fc18177ae69f84fbb60ecb84479775414ece3d
acilsd <acilsd@derpy.ru>
2019-01-04 01:07:13 +0300

    Express hello world.

--- sec_2/app.js
+++ sec_2/app.js
@@ -1,1 +1,15 @@
-//keep
+const express = require('express');
+const http = require('http');
+
+const PORT = '8087';
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.send('<h1>hello world</h1>');
+  // next();
+});
+
+const server = http.createServer(app);
+
+server.listen(PORT);
