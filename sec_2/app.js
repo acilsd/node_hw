6b162d90aa712a0f3e9b91fff36d7de0ea229675
acilsd <acilsd@derpy.ru>
2019-01-04 04:10:34 +0300

    Serving html files.

--- sec_2/app.js
+++ sec_2/app.js
@@ -7,6 +7,7 @@ const shopRoutes = require('./routes/shop');
 const PORT = '8087';
 
 const app = express();
+const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -14,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>404 error</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(PORT);
