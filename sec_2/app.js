de11300ff2ce0189c678580f4533089eab0305ab
acilsd <acilsd@derpy.ru>
2019-01-04 03:58:19 +0300

    Basic express routing

--- sec_2/app.js
+++ sec_2/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const PORT = '8087';
 
 const app = express();
 
-app.use('/add', (req, res, next) => {
-  res.send('<h1>Add something</h1>');
-  // next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Index</h1>');
-  // next();
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404 error</h1>');
 });
 
 app.listen(PORT);
